a18aceb37275b3805135d15fe673f35f7dd95b5e
sw1kwon <kwonsoonwon0628@gmail.com>
2024-11-27 19:08:20 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+
+  configureWebpack: {
+    entry: './src/main.ts', // main.ts를 엔트리 파일로 지정
+    resolve: {
+      extensions: ['.ts', '.js', '.vue', '.json'], // .ts 확장자를 포함해 Webpack이 처리할 파일 설정
+    },
+  },
+});
