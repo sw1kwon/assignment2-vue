1d408787a475db3e29955f780b477697f6268a0a
sw1kwon <kwonsoonwon0628@gmail.com>
2024-11-27 23:54:53 +0900

    Update package.json 0.2.6

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from '@vue/cli-service';
+const { defineConfig } = require('@vue/cli-service');
 
-export default defineConfig({
+module.exports = defineConfig({
   transpileDependencies: true, // Vue CLI에서 지원하는 옵션
 
-  configureWebpack: (config) => {
-    // Webpack 설정을 덮어씌움
-    config.entry = './src/main.ts'; // 엔트리 파일 설정
-    config.resolve = {
+  configureWebpack: {
+    entry: './src/main.ts', // 엔트리 파일 설정
+    resolve: {
       extensions: ['.ts', '.js', '.vue', '.json'] // 처리할 확장자 설정
-    };
+    }
   }
 });
